92e61e9717117cccd45a5c93aa0b7782cc13f4ba
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-04-21 11:02:14 +0700

    success stream from postman

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,3 @@
-use serde_json::json;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
@@ -7,50 +6,9 @@ use tokio::{
 pub struct Client {}
 
 impl Client {
-    pub async fn perform(is_stream: bool) {
-        let json_body;
+    pub async fn perform(is_stream: bool, json_body: &str, mut client_stream: TcpStream) {
         let request;
         if !is_stream {
-            json_body = json!({
-                  "model": "gemma3:1b",
-            "prompt": "hai"
-                  })
-            .to_string();
-            request = format!(
-                "POST /api/generate HTTP/1.1\r\n\
-            Host: localhost:11434\r\n\
-            Content-Type: application/json\r\n\
-            Content-Length: {}\r\n\
-            Connection: keep-alive\r\n\
-            \r\n\
-            {}",
-                json_body.len(),
-                json_body
-            );
-            let mut stream = TcpStream::connect("127.0.0.1:11434")
-                .await
-                .expect("error handshake");
-            stream
-                .write_all(request.as_bytes())
-                .await
-                .expect("error write");
-
-            let mut buffer = Vec::new();
-            stream
-                .read_to_end(&mut buffer)
-                .await
-                .expect("Error read stream");
-
-            let response = String::from_utf8_lossy(&buffer);
-            println!("Receveived {}", response);
-        } else {
-            json_body = json!({
-                  "model": "gemma3:1b",
-            "prompt": "hai",
-                 "stream": is_stream
-                  })
-            .to_string();
-
             request = format!(
                 "POST /api/generate HTTP/1.1\r\n\
             Host: localhost:11434\r\n\
@@ -62,26 +20,78 @@ impl Client {
                 json_body.len(),
                 json_body
             );
-            let mut stream = TcpStream::connect("127.0.0.1:11434")
+            let mut llm_stream = TcpStream::connect("127.0.0.1:11434")
                 .await
                 .expect("error handshake");
-            stream
+            llm_stream
                 .write_all(request.as_bytes())
                 .await
                 .expect("error write");
 
-            // Process streaming response
-            let mut buffer = [0; 1024];
-            loop {
-                let bytes_read = stream.read(&mut buffer).await.expect("Error read response");
-                if bytes_read == 0 {
-                    break;
-                }
+            let mut buffer = Vec::new();
+            llm_stream
+                .read_to_end(&mut buffer)
+                .await
+                .expect("Error read stream");
 
-                let chunk = &buffer[..bytes_read];
-                if let Ok(text) = std::str::from_utf8(chunk) {
-                    println!("{}", text);
-                }
+            let response = String::from_utf8_lossy(&buffer);
+            println!("Receveived {}", response);
+            client_stream
+                .write_all(&buffer)
+                .await
+                .expect("Eerror chunk write");
+
+            let _ = client_stream.flush().await;
+        } else {
+            request = format!(
+                "POST /api/generate HTTP/1.1\r\n\
+            Host: localhost:11434\r\n\
+            Content-Type: application/json\r\n\
+            Content-Length: {}\r\n\
+            Connection: keep-alive\r\n\
+            \r\n\
+            {}",
+                json_body.len(),
+                json_body
+            );
+            let mut llm_stream = TcpStream::connect("127.0.0.1:11434")
+                .await
+                .expect("error handshake");
+            println!("connected");
+            llm_stream
+                .write_all(request.as_bytes())
+                .await
+                .expect("error write");
+            println!("written");
+
+            let mut headers = [0u8; 1024];
+            let header_len = llm_stream.read(&mut headers).await.expect("header error");
+            client_stream
+                .write_all(&headers[..header_len])
+                .await
+                .expect("Error write once");
+
+            // Process streaming response
+            let mut chunk = [0; 1024];
+            loop {
+                let bytes_read = match llm_stream.read(&mut chunk).await {
+                    Ok(0) => {
+                        println!("closed llm connection");
+                        break;
+                    } // Connection closed by Ollama
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Read error: {}", e);
+                        break;
+                    }
+                };
+
+                client_stream
+                    .write_all(&chunk[..bytes_read])
+                    .await
+                    .expect("Eerror chunk write");
+
+                let _ = client_stream.flush().await;
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,75 @@
 use std::io::Error;
 use stockbit_chatbot::client::Client;
-use tokio::net::{TcpListener, TcpSocket, TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let is_stream = true;
-    println!("Hello, world!");
-    Client::perform(is_stream).await;
-    let listener = TcpListener::bind("127.0.0.1:8080")
+    let listener = TcpListener::bind("localhost:8080")
         .await
         .expect("bind port error");
+    println!("Server running on http://localhost:8080");
 
-    let (stream, _) = listener.accept().await.expect("error accept");
-
-    tokio::spawn(async move {
-        if let Err(e) = handle_client(stream).await {
-            eprintln!("connection error {}", e);
-        }
-    });
-
-    Ok(())
+    loop {
+        let (stream, _) = listener.accept().await.expect("error accept");
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(stream).await {
+                eprintln!("connection error {}", e);
+            }
+        });
+    }
 }
 
-async fn handle_client(stream: TcpStream) -> Result<(), Error> {
+async fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    let mut buffer = [0; 1024];
+    // let size = stream.read(&mut buffer).await?;
+    match stream.read(&mut buffer).await {
+        Ok(size) => {
+            let request = String::from_utf8_lossy(&buffer[..size]);
+            if request.starts_with("OPTIONS") {
+                let response = "HTTP/1.1 204 No Content\r\n\
+            Access-Control-Allow-Origin: *\r\n\
+            Access-Control-Allow-Methods: POST, GET, OPTIONS\r\n\
+            Access-Control-Allow-Headers: Content-Type\r\n\
+            Access-Control-Max-Age: 86400\r\n\
+            \r\n";
+
+                stream.write_all(response.as_bytes()).await?;
+                return Ok(());
+            }
+            if request.starts_with("POST /chatbot") {
+                // let response_headers = "HTTP/1.1 200 OK\r\n\
+                // Access-Control-Allow-Origin: *\r\n\
+                // Content-Type: text/plain\r\n\
+                // Transfer-Encoding: chunked\r\n\
+                // \r\n";
+                //
+                // stream.write_all(response_headers.as_bytes()).await?;
+                println!("request {}", request);
+                let mut parts = request.split("\r\n\r\n");
+                let _ = parts.next().expect("no req head ");
+                let body = parts.next().expect("no body");
+                Client::perform(true, body, stream).await;
+            } else {
+                stream
+                    .write_all(
+                        format!("{}{}", "HTTP/1.1 404 Not Found\r\n\r\n", "404 Not Found")
+                            .as_bytes(),
+                    )
+                    .await
+                    .expect("Error request url");
+            }
+        }
+        Err(_) => {
+            stream
+                .write_all(
+                    format!("{}{}", "HTTP/1.1 404 Not Found\r\n\r\n", "404 Not Found").as_bytes(),
+                )
+                .await
+                .expect("Error request url");
+        }
+    }
     Ok(())
 }
